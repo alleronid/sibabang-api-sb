a9a9e5cfcf0586430c2f335edf72040a7eb28c64
alief <alief.bagas@pinjamyuk.co.id>
2025-03-18 08:58:23 +0700

    add index url

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.use('/api', routes)
 
 app.all('*', (req, res, next) => {
